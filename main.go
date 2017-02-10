f07c161e5f993d449d8d9458255b348188f63289
Kevin Graney <nanonet@gmail.com>
2017-02-10 22:22:28 +0000

    Add SSL

--- main.go
+++ main.go
@@ -22,6 +22,9 @@ func generateRandomPage(pageSize int) (string, string) {
 }
 
 func runServer(c *cli.Context) error {
+	certFile := c.String("cert")
+	keyFile := c.String("key")
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Request for URL: /%s", r.URL.Path[1:])
 		hash, bstr := generateRandomPage(184643)
@@ -30,7 +33,7 @@ func runServer(c *cli.Context) error {
 	})
 
 	log.Print("Starting listening server")
-	return http.ListenAndServe(":8080", nil)
+	return http.ListenAndServeTLS(":8080", certFile, keyFile, nil)
 }
 
 func sendRequests(c *cli.Context) error {
@@ -58,7 +61,7 @@ func sendRequests(c *cli.Context) error {
 
 	for {
 		_, urlRand := generateRandomPage(30)
-		url := fmt.Sprintf("http://%s/%s", server, urlRand)
+		url := fmt.Sprintf("https://%s/%s", server, urlRand)
 		urls <- url
 	}
 
@@ -92,6 +95,16 @@ func main() {
 			Name:   "server",
 			Usage:  "Start a local random page server",
 			Action: runServer,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "cert",
+					Usage: "The SSL certificate file",
+				},
+				cli.StringFlag{
+					Name:  "key",
+					Usage: "The SSL key file",
+				},
+			},
 		},
 	}
 
